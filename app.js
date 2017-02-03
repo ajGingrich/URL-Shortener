120f3866e62321f6760b42c413b49191094ed658
Andrew Gingrich <ajgingrich@yahoo.com>
2017-02-03 09:17:12 -0600

    first working version

--- app.js
+++ app.js
@@ -1,29 +1,13 @@
 var express = require('express');
 var path = require('path');
+var validUrl = require('valid-url');
 
 /*-------MongoDB----------*/
 var mongodb = require('mongodb');
 var MongoClient = mongodb.MongoClient;
 
 // Connection URL. This is where your mongodb server is running.
-
 var url = process.env.MONGOLAB_URI;
-
-console.log(url);
-
-// Use connect method to connect to the Server
-MongoClient.connect(url, function (err, db) {
-    if (err) {
-        console.log('Unable to connect to the mongoDB server. Error:', err);
-    } else {
-        console.log('Connection established to', url);
-
-        // do some work here with the database.
-
-        //Close connection
-        db.close();
-    }
-});
 
 var app = express();
 
@@ -39,8 +23,95 @@ app.use(express.static(__dirname + '/public'));
 //define routes
 //home
 app.get('/', function (req, res) {
-
     res.render('index')
+});
+
+//catch the favicon first
+app.get('/favicon.ico', function(req, res) {
+    //var input = req.params.id;
+    res.json(204);
+});
+
+var info = {original_url: "", short_url: ""};
+
+//get new URL, add to database and send
+app.get('/new/:url*', function (req, res) {
+    var input = req.params['url'] + req.params[0];
+    info.original_url = input;
+    var randomNum =  Math.floor(100000 + Math.random() * 900000);
+
+    //regex.test(input);
+    if (validUrl.isUri(input)) {
+        //info.short_url = "http://localhost:5000/"  + randomNum;
+        info.short_url = "https://urlshortener320.herokuapp.com/" + randomNum;
+
+        // Use connect method and insert to the Server
+        MongoClient.connect(url, function (err, db) {
+            if (err) {
+                console.log('Unable to connect to the mongoDB server. Error:', err);
+            } else {
+                var collection = db.collection('urls');
+
+                collection.insert(info, function(err, data) {
+                    if (err) {
+                        return err;
+                    }
+                    else {
+                        console.log(data);
+                    }
+                });
+
+                //Close connection
+                db.close();
+            }
+        });
+        info = {original_url: input, short_url:  "http://localhost:5000/"  + randomNum};
+    }
+    else {
+        info = {error: "Please enter a URL in the correct format as shown in the instructions"}
+    }
+
+    res.json(info);
+});
+
+//redirect
+app.get('/:id', function (req, res){
+    var number = req.params.id;
+
+    if (/^\d+$/.test(number)) {
+        //check that number is in db.
+        MongoClient.connect(url, function (err, db) {
+            if (err) {
+                console.log('Unable to connect to the mongoDB server. Error:', err);
+            } else {
+                var collection = db.collection('urls');
+
+                collection.find({
+                    short_url: 'https://urlshortener320.herokuapp.com/' + number
+                    //short_url: 'http://localhost:5000/' + number
+                }).toArray(function(err, documents) {
+                    if (err) {
+                        return err;
+                    }
+
+                    res.writeHead(301,
+                        {Location: documents[0].original_url}
+                    );
+                    res.end();
+
+                });
+
+                //Close connection
+                db.close();
+            }
+
+        });
+    }
+    else {
+        var errorMessage = "please enter a valid number";
+        res.json(errorMessage);
+    }
+
 });
 
 app.listen(app.get('port'), function() {
